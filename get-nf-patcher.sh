9145c4faa5ff949ff4354be58df2149830862b28
bri <284789+b-@users.noreply.github.com>
2023-02-09 03:31:04 -0500

    Update get-nf-patcher.sh

--- get-nf-patcher.sh
+++ get-nf-patcher.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-git clone \
-  --depth 1 \
-  --single-branch \
-  --quiet --progress \
-  --no-checkout \
-  https://github.com/ryanoasis/nerd-fonts
-cd nerd-fonts && git checkout HEAD src font-patcher
+#git clone \
+#  --depth 1 \
+#  --single-branch \
+#  --quiet --progress \
+#  --no-checkout \
+#  https://github.com/ryanoasis/nerd-fonts
+#cd nerd-fonts && git checkout HEAD src font-patcher
+
+FONTPATCHER_ZIP=https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FontPatcher.zip
+FONTPATCHER_ZIP=https://github.com/b-/nerd-fonts/raw/release-master-fontpatcher/FontPatcher.zip
+
+mkdir nerd-fonts
+cd nerd-fonts
+wget "$FONTPATCHER_ZIP"
+unzip FontPatcher.zip
